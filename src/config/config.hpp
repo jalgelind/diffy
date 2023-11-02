88b74abb3792e57acddc687a85fafc90a2ea9fd4
Johannes Algelind <johannes@algelind.se>
2023-11-02 02:43:35 +0100

    fix: default options make testsuite fail; revert
    it should be possible to override and set those values explicitly
    
    they can't be toggled off when enabled in config

--- src/config/config.hpp
+++ src/config/config.hpp
@@ -25,8 +25,8 @@ struct ProgramOptions {
 
     std::string theme = "theme_default";
 
-    bool ignore_line_endings = true;
-    bool ignore_whitespace = true;
+    bool ignore_line_endings = false;
+    bool ignore_whitespace = false;
 
     std::string left_file;
     std::string right_file;
